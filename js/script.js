bc36d016407991773edf87a96416faef76e9ca9a
D3HuCoB_E7OP <155985369+D3N1S0V-G3ORG3@users.noreply.github.com>
2025-09-15 11:56:37 +0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@
 // API Configuration
 // API Configuration
 const API_CONFIG = {
-  baseURL: "https://ee2ed0efe040.ngrok-free.app/api/v1",
+  baseURL: "https://109d84fe7ce1.ngrok-free.app/api/v1",
   
   endpoints: {
     uploadImage: "/upload/image",
@@ -19,7 +19,7 @@ const API_CONFIG = {
     checkFaceCount: "/analysis/faces"
   },
   
-  wsURL: "wss://ee2ed0efe040.ngrok-free.app/ws"
+  wsURL: "wss://109d84fe7ce1.ngrok-free.app/ws"
 };
   
 // Определение платформы
